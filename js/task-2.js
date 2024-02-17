8263e94f609c0ed4435a89d5bf875a246b58d396
Valentyn <0938442244vv@gmail.com>
2024-02-17 02:50:35 +0200

    tasc 2

--- js/task-2.js
+++ js/task-2.js
@@ -1,1 +1,13 @@
-dghgg
+function calcAverageCalories(days) {
+    let totalCalories = 0;
+    for (const day of days) {
+        totalCalories += day.calories;
+    }
+    let averageDaysCalories = 0;
+    if (days.length === 0) {
+        return 0;
+    } else { 
+        averageDaysCalories = totalCalories / days.length;
+        return averageDaysCalories;
+    }
+}
