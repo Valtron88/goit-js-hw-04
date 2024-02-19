c2905be1f27515d5d86e6f45a85a2fd2c7b2d277
Valentyn <0938442244vv@gmail.com>
2024-02-19 06:03:12 +0200

    fin

--- js/task-3.js
+++ js/task-3.js
@@ -12,3 +12,10 @@ const profile = {
     }
 };
 
+console.log(profile.getInfo()); 
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); 
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo());
